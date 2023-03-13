6538beb906749e2e72f1939eedb53517c5c00fe6
Ali <ali.frankel@gmail.com>
2023-03-12 21:09:00 -0400

    half of part 3 done

--- api/posts.js
+++ api/posts.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const postsRouter = express.Router();
 
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
+});
+
 postsRouter.use((req, res, next) => {
     console.log("a request is being made to /posts");
     next();
